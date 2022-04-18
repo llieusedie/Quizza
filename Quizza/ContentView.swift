5a5d93cfb3e2cb26e5b9f0e8aff91914270fa67d
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-04-18 19:53:20 +0300

    Initial Commit

---
+++ Quizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quizza
+//
+//  Created by Paul Kirnoz on 18.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quizza/QuizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzaApp.swift
+//  Quizza
+//
+//  Created by Paul Kirnoz on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
