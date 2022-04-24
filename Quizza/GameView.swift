517ecd36ac824524d3b260dc4fa21084c3175e6c
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-04-25 01:44:08 +0300

    initial commit

--- Quizza/GameView.swift
+++ Quizza/GameView.swift
@@ -1,136 +1,51 @@
 //
-//  ContentView.swift
+//  GameView.swift
 //  Quizza
 //
-//  Created by Paul Kirnoz on 18.04.2022.
-//README:
-/*
- 1. Load custom fonts (Jost) DONE!
- 
- 2. Add tabView with tab Items
- 2.1. Create a page for each tab
- 
- 3. Create array of questions under each of topics
- 
- 4. Create logic
- 4.1. create working prototype
- 4.2. Button, Timer, Pause/Unpause, Refresh
- 
- 5. Create a custom swipe action to change topics
- -horizontalScrollView/SwipeGesture
- 
- 6. Create a pallette for each topic: "Nature", "Science", "Sports", "Misc", "Arts", "Geography"
- 6.1. attach a pallette to each topic (dictionary? custom struct/class property?) Google: "custom property"
- 7. Create foreach for each topic
- */
+//  Created by Paul Kirnoz on 22.04.2022.
+//
 
+import Foundation
 import SwiftUI
-import Combine
 
-
-struct CustomColors {
-    static let natureColor = Color("Nature")
-    static let natureColorBackground = Color("NatureBackground")
-}
-
-struct ContentView: View {
+struct GameView: View {
     
-    let topics = ["Nature", "Science", "Sports", "Misc", "Arts", "Geography"]
-    @State private var currentTopic = "Nature"
-    @State private var currentTitle = "Quizza"
-    @State private var currentButtonTitle = "START"
-    @State private var question = "Name types of trees"
-    @State private var animationAmount = 1.0
-    @State private var currentButtonColor = CustomColors.natureColor
     @State private var currentBackgroundColor = CustomColors.natureColorBackground
-    
-    //MARK: Timer
-    
-    @State private var isHidden = true
+    @State private var currentButtonColor = CustomColors.natureColor
+    @State private var animationAmount = 1.0
+    @State private var currentTopic = "Nature"
     @State private var timeRemaining = 60
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State var isTimerRunning = false
     @State private var startTime =  Date()
     @State private var timerString = "0.00"
-    @State private var playButtonHidden = true
-    @State private var connectedTimer: Cancellable? = nil
     
-    func instantiateTimer() {
-        self.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-        
-        return
-    }
-    
-    func cancelTimer() {
-        self.connectedTimer?.cancel()
-        return
-    }
-    
-    func resetCounter() {
-        self.timeRemaining = 60
-        return
-    }
-    
-    func restartTimer() {
-        self.resetCounter()
-        self.cancelTimer()
-        self.instantiateTimer()
-        return
-    }
     
     func stopTimer() {
         self.timer.upstream.connect().cancel()
-        
     }
     
     func startTimer() {
         self.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     }
     
-    func gameStarts() {
-//        withAnimation(.interpolatingSpring(stiffness: 10, damping: 5)) {
-        withAnimation(.easeInOut(duration: 1)) {
-            currentTitle = ""
-            currentTopic = question
-            currentButtonTitle = ""
-            restartTimer()
-        }
-    }
     
-    func gameEnds() {
-        withAnimation(.interpolatingSpring(stiffness: 10, damping: 5)) {
-            currentTitle = "Quizza"
-            currentTopic = "Nature"
-            currentButtonTitle = "START"
-            stopTimer()
-            withAnimation(.easeInOut) {
-            isHidden.toggle()
-            }
-            restartTimer()
-        }
-        
-    }
     
     var body: some View {
-        
         NavigationView {
             ZStack {
-                Color(UIColor(currentBackgroundColor)).ignoresSafeArea() //background color
+                Color(UIColor(currentBackgroundColor)).ignoresSafeArea()
                 
                 VStack {
-                    VStack(spacing: 10) {
-                        
-                        Text(currentTitle)
-                            .foregroundColor(.white)
-                            .font(.custom("Jost-SemiBoldItalic", size: 60))
-                        
-                        Text(currentTopic)
-                            .padding([.leading, .trailing])
-                            .foregroundColor(.white)
-                            .font(.custom("Jost-LightItalic", size: 30))
-                    }
+                    Text("What's your name?")
+                        .padding([.leading, .trailing])
+                        .foregroundColor(.white)
+                        .font(.custom("Jost-LightItalic", size: 30))
+                    
                     Spacer()
+                    
                     ZStack {
+                        
                         ZStack {
                             Circle()
                                 .frame(width: 200, height: 200, alignment: .center)
@@ -178,8 +93,8 @@ struct ContentView: View {
                                     animationAmount = 2
                                 }
                         }
+                        
                         Text("\(timeRemaining)")
-                            .opacity(isHidden ? 0 : 1)
                             .font(.custom("Jost-SemiBold", size: 35))
                             .foregroundColor(.white)
                             .onReceive(timer) { time in
@@ -187,39 +102,26 @@ struct ContentView: View {
                                     timeRemaining -= 1
                                 }
                             }
-                        
                             .onAppear() {
                                 // no need for UI updates at startup
                                 self.stopTimer()
                             }
-                        
-                        Button(currentButtonTitle) {
-                            
-                            gameStarts()
-                            withAnimation(.easeInOut) {
-                            isHidden.toggle()
-                            }
-                            playButtonHidden.toggle()
-                        }
-                        .foregroundColor(.white)
-                        .font(.custom("Jost-SemiBold", size: 30))
                     }
-                    Spacer()
                     Spacer()
                     Spacer()
                 }
             }
+           
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         if isTimerRunning {
-                        
+                            // stop UI updates
                             self.stopTimer()
-                            
                         } else {
-                            timerString = timerString.self
-                            
-                            
+                            timerString = "60"
+                            startTime = Date()
+                            // start UI updates
                             self.startTimer()
                         }
                         isTimerRunning.toggle()
@@ -234,30 +136,19 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                         }
                     }
-                    .opacity(isHidden ? 0 : 1)
                 }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        gameEnds()
-                    } label: {
-                        Image(systemName: "arrow.backward")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                    }
-                    
-                    .opacity(isHidden ? 0 : 1)
-                    
-                }
-                
             }
+            
+            
+            
         }
+        
     }
 }
 
-
-struct ContentView_Previews: PreviewProvider {
+struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GameView()
             .previewInterfaceOrientation(.portrait)
     }
 }
